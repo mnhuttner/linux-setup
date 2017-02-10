496b4e2929e3cc5698c81a41845d0e19e881a03a
Michael Huttner <mhuttner@gmail.com>
2017-02-10 01:05:18 -0500

    more

--- setup.sh
+++ setup.sh
@@ -4,6 +4,8 @@ yum -y update
 
 yum -y install https://sourceforge.net/projects/etherape/files/etherape/0.9.14/etherape-0.9.14-obs.centos700.26.1.x86_64.rpm
 
+yum -y install https://kojipkgs.fedoraproject.org//packages/system-config-lvm/1.1.18/1.fc19/noarch/system-config-lvm-1.1.18-1.fc19.noarch.rpm
+
 for i in $(cat packages.txt) ; do
   yum -y install $i
 done
